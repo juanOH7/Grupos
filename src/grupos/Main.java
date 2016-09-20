6f6dd722d883e99d50917e63c7caf27998f4b27f
Juan O'Hara <Juan O'Hara@PC-OTAKU>
2016-09-20 02:36:55 -0600

    Cargar

--- src/grupos/Main.java
+++ src/grupos/Main.java
@@ -2,6 +2,7 @@ package grupos;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -10,15 +11,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        File arc = new File("miembros.txt");
-
+        File arc = new File("./miembros.txt");
+        File gur = new File("./guardar.txt");
         Scanner userSC = new Scanner(System.in);
-        Scanner sc = new Scanner(arc);
+        Scanner sc;
 
-        int numInt, numGr, sobra, totPEr = 0, totParejas = 0;
+        int numInt, numGr, sobra, numTur, totPEr = 0, totParejas = 0;
         String REsPUs;
         boolean forcedCouple;
         boolean oneCouple;
+        boolean cont = false;
         ArrayList<Integer> numXgru = new ArrayList();
 
         LinkedList<Persona> personas = new LinkedList();
@@ -30,15 +32,37 @@ public class Main {
 
         ArrayList<Grupo> grupos = new ArrayList();
         Persona tmp;
-        while (sc.hasNextLine()) {
-            String temp = sc.nextLine();
-            String div[] = temp.split(":");
-            if (div[0].contains(",")) {
-                tmp = new Persona(div[0], true, Integer.valueOf(div[1]));
-                personas.add(tmp);
-            } else {
-                tmp = new Persona(div[0], false, Integer.valueOf(div[1]));
-                personas.add(tmp);
+        if (gur.exists()) {
+            System.out.println("Continuar [S/N]");
+            REsPUs = userSC.next();
+            cont = (REsPUs.equals("S") || REsPUs.equals("s"));
+        }
+
+        if (!cont) {
+            sc = new Scanner(arc);
+            while (sc.hasNextLine()) {
+                String temp = sc.nextLine();
+                String div[] = temp.split(":");
+                if (div[0].contains(",")) {
+                    tmp = new Persona(div[0], true, Integer.valueOf(div[1]));
+                    personas.add(tmp);
+                } else {
+                    tmp = new Persona(div[0], false, Integer.valueOf(div[1]));
+                    personas.add(tmp);
+                }
+            }
+        } else {
+            sc = new Scanner(gur);
+            while (sc.hasNextLine()) {
+                String temp = sc.nextLine();
+                String div[] = temp.split(":");
+                if (div[0].contains(",")) {
+                    tmp = new Persona(div[0], true, Integer.valueOf(div[1]));
+                    personas.add(tmp);
+                } else {
+                    tmp = new Persona(div[0], false, Integer.valueOf(div[1]));
+                    personas.add(tmp);
+                }
             }
         }
 
@@ -55,6 +79,9 @@ public class Main {
 
         System.out.println("¿Una Pareja por grupo? [S/N]: ");
         REsPUs = userSC.next();
+
+        System.out.println("Número de turnos:");
+        numTur = userSC.nextInt();
 
         for (int i = 0; i < personas.size(); i++) {
             if (personas.get(i).isIsCouple()) {
@@ -86,19 +113,16 @@ public class Main {
         Collections.shuffle(candidatosLider);
         for (int i = 0; i < numGr; i++) {
             lideres.add(candidatosLider.get(i));
-            lideres.get(i).setTurns(2);
+            lideres.get(i).setTurns(numTur);
             lideres.get(i).setAssignado(true);
             if (candidatosLider.get(i).isIsCouple()) {
                 totParejas -= 1;
             }
         }
 
-        for (int i = 0; i < lideres.size(); i++) {
-        }
         for (int i = 0; i < numGr; i++) {
             grupos.add(new Grupo(numXgru.get(i)));
         }
-
 
         for (int i = 0; i < numGr; i++) {
             grupos.get(i).add(lideres.get(i));
@@ -151,7 +175,7 @@ public class Main {
                 indice++;
             }
         }
-        
+
         for (int i = 0; i < integrantes.size(); i++) {
             if (integrantes.get(i).getTurns() != 0) {
                 integrantes.get(i).setTurns(integrantes.get(i).getTurns() - 1);
@@ -162,6 +186,12 @@ public class Main {
             System.out.println("Grupo" + (i + 1));
             System.out.println(grupos.get(i).getIntegrantes());
         }
+
+        PrintWriter bw = new PrintWriter(new File("guardar.txt"));
+        for (int i = 0; i < personas.size(); i++) {
+            bw.write(personas.get(i).getName() + ":" + personas.get(i).getTurns() + "\n");
+        }
+        bw.close();
 
     }
 
